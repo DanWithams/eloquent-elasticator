cbd195e1b3d4c1419a9ccb9e01fd9140af124d5e
DanWithams <me@danwithams.com>
2022-04-20 19:36:26 +0100

    Reverting

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -2,7 +2,6 @@
 
 namespace DanWithams\EloquentElasticator;
 
-use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Traits\ForwardsCalls;
 use DanWithams\EloquentElasticator\Models\Sort;
@@ -10,8 +9,8 @@ use DanWithams\EloquentElasticator\Models\Query;
 use DanWithams\EloquentElasticator\Models\Field;
 use DanWithams\EloquentElasticator\Concerns\Client;
 use DanWithams\EloquentElasticator\Models\SortItem;
-use DanWithams\EloquentElasticator\Models\MultiMatch;
 use DanWithams\EloquentElasticator\Models\QueryString;
+use DanWithams\EloquentElasticator\Models\Contracts\QueryCriteria;
 
 class QueryBuilder
 {
@@ -19,15 +18,12 @@ class QueryBuilder
 
     protected string $index;
     protected Builder $query;
-    protected Collection $criteria;
+    protected QueryCriteria $criteria;
     protected Sort $sort;
 
     public function __construct(protected string $model)
     {
-        $this->criteria = collect([
-            new QueryString(),
-            new MultiMatch()
-        ]);
+        $this->criteria = new QueryString();
         $this->sort = new Sort();
         $this->index = (new $this->model)->elasticatableAs();
         $this->query = call_user_func($this->model . '::query');
@@ -42,21 +38,21 @@ class QueryBuilder
 
     public function whereField($field, $boost = null): self
     {
-        $this->criteria->each->addField(new Field($field, $boost));
+        $this->criteria->addField(new Field($field, $boost));
 
         return $this;
     }
 
     public function fuzzy($fuzziness = 'AUTO'): self
     {
-        $this->criteria->each->setFuzziness($fuzziness);
+        $this->criteria->setFuzziness($fuzziness);
 
         return $this;
     }
 
     public function matches($queryString): self
     {
-        $this->criteria->each->setQueryString($queryString);
+        $this->criteria->setQueryString($queryString);
 
         return $this;
     }
@@ -72,24 +68,20 @@ class QueryBuilder
 
     public function get()
     {
-        if ($this->criteria->first()->getQueryString()) {
+        if ($this->criteria->getQueryString()) {
+            $client = app(Client::class, ['index' => $this->index]);
 
+            $body = [
+                'query' => (new Query())
+                    ->setMatch($this->criteria)
+                    ->toArray(),
+            ];
 
-            $documents = $this->criteria->map(function () {
-                $client = app(Client::class, ['index' => $this->index]);
+            if ($this->sort->count()) {
+                $body['sort'] = $this->sort->toArray();
+            }
 
-                $body = [
-                    'query' => (new Query())
-                        ->setMatch($this->criteria)
-                        ->toArray(),
-                ];
-
-                if ($this->sort->count()) {
-                    $body['sort'] = $this->sort->toArray();
-                }
-
-                return $client->query($body);
-            })->flatten(1);
+            $documents = $client->query($body);
 
             $ids = collect(data_get($documents, 'hits.hits'))
                 ->pluck('_id');
