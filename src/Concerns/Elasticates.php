f16f59b437b5c66dd47bc7a477a203c82e25d74f
DanWithams <me@danwithams.com>
2022-04-20 16:16:28 +0100

    Adding index removal

--- src/Concerns/Elasticates.php
+++ src/Concerns/Elasticates.php
@@ -8,11 +8,12 @@ trait Elasticates
 {
     public function elasticate(): self
     {
+        $client = app(Client::class, ['index' => $this->elasticatableAs()]);
+        $body = $this->toElastic();
         if ($this->shouldElasticate()) {
-            $index = $this->elasticatableAs();
-            $body = $this->toElastic();
-            $client = app(Client::class, ['index' => $index]);
             $client->index($body);
+        } else {
+            $client->delete($body['id']);
         }
 
         return $this;
